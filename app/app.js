e08292e534895e2fc9a1122540374d41d59d5b50
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-03-23 14:12:39 +0000

    initial commit

--- app/app.js
+++ app/app.js
@@ -7,6 +7,14 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  engines = {
+    foo: {
+      dependencies: {
+        services: ['router']
+      }
+    }
+  }
 }
 
 loadInitializers(App, config.modulePrefix);
